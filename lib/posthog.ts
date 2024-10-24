cbc5f0d2a290047e42bf7b674fb7645bdb5bd9f5
David Rhodes <david@espressocode.tech>
2024-10-24 13:17:46 -0500

    correct env variables

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -3,8 +3,9 @@
 import posthog from 'posthog-js'
 
 if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+  posthog.init(process.env.POSTHOG_KEY as string, {
+    api_host: process.env.POSTHOG_HOST || 'https://app.posthog.com',
+    person_profiles: 'identified_only',
     loaded: (posthog) => {
       if (process.env.NODE_ENV === 'development') posthog.debug()
     },
