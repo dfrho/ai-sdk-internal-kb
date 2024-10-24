b680b4acb1a4037e013e8a69b9b3d20e2714c119
David Rhodes <david@espressocode.tech>
2024-10-24 13:22:03 -0500

    corrects chained optional host

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -4,7 +4,7 @@ import posthog from 'posthog-js'
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.POSTHOG_KEY as string, {
-    api_host: process.env.POSTHOG_HOST || 'https://app.posthog.com',
+    api_host: process.env.POSTHOG_HOST || 'https://us.i.posthog.com',
     person_profiles: 'identified_only',
     loaded: (posthog) => {
       if (process.env.NODE_ENV === 'development') posthog.debug()
