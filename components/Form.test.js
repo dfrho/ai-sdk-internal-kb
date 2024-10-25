da00e7eec320d300687d980e3117b4daa44a34ad
David Rhodes <david@espressocode.tech>
2024-10-25 11:59:35 -0500

    adds react testing for form

---
+++ components/Form.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Form } from './form';
+
+test('renders the Form text', () => {
+  render(<Form />);
+  const textElement = screen.getByText(/Email Address/i);
+  expect(textElement).toBeInTheDocument();
+});
